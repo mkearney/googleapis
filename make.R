fc354c385c045b2b117a51614a185b1b369aafa3
Kearney <kearneymw@mu-132181.dhcp.missouri.edu>
2017-08-29 16:01:48 -0500

    cleaned up make.R

--- make.R
+++ make.R
@@ -1,9 +1,12 @@
+## build funs
 devtools::load_all()
-api_call("asdf")
+devtools::document()
 
+## fetch text to analyze
 rt <- rtweet::search_tweets("trump is the worst president", include_rts = FALSE)
 text <- rt$text[1:5]
 
+## sentiment analysis
 x <- analyze_sentiment(text)
 x
 
